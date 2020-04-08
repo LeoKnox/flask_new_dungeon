ecba90084a6a7ff668243462a7e214c71fa9e004
LeoKnox <LeoKnox@users.noreply.github.com>
2020-04-07 18:04:19 -0700

    Add files via upload

---
+++ dungeon.py
@@ -0,0 +1,29 @@
+import os
+
+from flask import Flask
+from flask import render_template, request
+
+from flask_sqlalchemy import SQLAlchemy
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "bookdatabase.db"))
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+
+db = SQLAlchemy(app)
+
+class Room(db.Model):
+    title = db.Column(db.String(30), unique=True, nullable=False, primary_key=True)
+
+def __repr__(self):
+    return"<Title: {}>".format(self.title)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.form:
+        print(request.form)
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
