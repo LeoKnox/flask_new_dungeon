fb6f717eda8e0aa21fbd565c0f03c3349bc354d6
LeoKnox <LeoKnox@users.noreply.github.com>
2020-04-08 21:32:25 -0700

    Add files via upload

--- run.py
+++ run.py
@@ -15,6 +15,17 @@ rooms = [
     }
 ]
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db. relationship('Post', backref='title', lazy=True)
+
+def __repr__(self):
+    return f"User('{self.username}', '{self.email}','{self.image_file}')"
+
 @app.route("/")
 def hello():
     return render_template('home.html', rooms=rooms)
