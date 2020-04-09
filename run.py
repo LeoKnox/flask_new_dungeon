dbb7797f73f1cfa34d7bea02a4b01130fd6b2269
LeoKnox <LeoKnox@users.noreply.github.com>
2020-04-08 21:21:05 -0700

    Add files via upload

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@127.0.0.1/databasename'
+
+db = SQLAlchemy(app)
 
 rooms = [
     {
